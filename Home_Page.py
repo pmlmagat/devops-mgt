d841f0d71f16facc7f3b6b2ae39df407c6a34127
pmlmagat <133682877+pmlmagat@users.noreply.github.com>
2023-10-17 20:26:58 +0800

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -5,7 +5,7 @@ st.set_page_config(
     page_icon="🏠",
 )
 
-# Add a CSS style to center the title
+# Add a CSS style to center the title and make it bigger
 st.markdown(
     """
     <style>
@@ -13,7 +13,11 @@ st.markdown(
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 80vh;
+        height: 100vh;
+    }
+
+    .bigger-title {
+        font-size: 36px; /* You can adjust the font size as needed */
     }
     </style>
     """,
@@ -21,6 +25,6 @@ st.markdown(
 )
 
 # Create a container div with the center class to center the title
-st.markdown('<div class="center"><h1>Time Classification</h1></div>', unsafe_allow_html=True)
+st.markdown('<div class="center"><h1 class="bigger-title">Time Classification</h1></div>', unsafe_allow_html=True)
 
 st.sidebar.success("Select page above.")
