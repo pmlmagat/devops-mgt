a01de9f3f833a4d7c041e1eb8f43d7910ad3e7c3
pmlmagat <133682877+pmlmagat@users.noreply.github.com>
2023-10-17 20:44:39 +0800

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -13,19 +13,22 @@ st.markdown(
         display: flex;
         justify-content: center;
         align-items: center;
+        flex-direction: column; /* Added to stack the emoji and title */
         height: 80vh;
     }
 
     .bigger-title {
         font-size: 56px; /* You can adjust the font size as needed */
     }
+    .emoji {
+        font-size: 72px; /* Adjust the size of the emoji as needed */
+    }
     </style>
     """,
     unsafe_allow_html=True,
 )
 
-
-# Create a container div with the center class to center the title
-st.markdown('<div class="center"><h1 class="bigger-title">Time Classification</h1></div>', unsafe_allow_html=True)
+# Create a container div with the center class to center the emoji and title
+st.markdown('<div class="center"><span class="emoji">🕓</span><h1 class="bigger-title">Time Classification</h1></div>', unsafe_allow_html=True)
 
 st.sidebar.success("Select page above.")
