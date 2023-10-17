5bbdc7eb9b845c8ed2698f6c0fd081cc3791f2ce
pmlmagat <133682877+pmlmagat@users.noreply.github.com>
2023-10-17 20:39:01 +0800

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -24,6 +24,13 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+# Upload a local GIF file
+uploaded_file = st.file_uploader("Upload a GIF", type=["gif"])
+
+if uploaded_file is not None:
+    # Display the uploaded GIF above the "Time Classification" text
+    st.image(uploaded_file, use_column_width=True)
+
 # Create a container div with the center class to center the title
 st.markdown('<div class="center"><h1 class="bigger-title">Time Classification</h1></div>', unsafe_allow_html=True)
 
